df692497df345cb751080c237348636d780541c9
robson soares <robsonvieira250@hotmail.com>
2023-10-22 21:20:31 -0300

    login

---
+++ script.js
@@ -0,0 +1,17 @@
+ 
+
+    let email = document.getElementById("e-mail");
+    let senha = document.getElementById("senha");
+
+    function login(){
+        if (email.value == 'robsonvieira@gmail.com'&& senha.value =='1234'){
+                window.alert ('Login feito com sucesso')
+        }else {
+            window.alert ('Email ou senha incorreta')
+        }
+
+
+    }
+
+ 
+
